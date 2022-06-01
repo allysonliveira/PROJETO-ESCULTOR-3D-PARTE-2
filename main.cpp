9f6c796c450c8a6d19ba8d93e54e07f6ea0c5837
allysonliveira <allysonmgo@gmail.com>
2022-06-01 05:27:38 -0300

    inital commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "sculptor.hpp"
+
+int main()
+{
+    /*
+    Sculptor steve(50,50,50);
+
+    //falta colorir corretamente
+
+    //cabeça
+    steve.setColor(1.0,1.0,1.0,1.0); // branco
+    steve.putBox(5,13,20,28,6,14);
+
+    //tronco
+    steve.setColor(0,0,0,1.0); // preto
+    steve.putBox(5,13,10,20,6,14);
+
+    //pernas
+    steve.setColor(1.0,1.0,1.0,1.0); // branco
+    steve.putBox(5,13,2,10,6,14);
+
+    //pes
+    steve.setColor(0,0,0,1.0); // preto
+    steve.putBox(5,13,0,2,6,15);
+
+    //braços
+
+    steve.setColor(1.0,1.0,1.0,1.0); // branco
+    steve.putBox(1,5,10,20,7,13);
+
+    steve.setColor(1.0,1.0,1.0,1.0); // branco
+    steve.putBox(13,17,10,20,7,13);
+
+
+    steve.writeOFF("steve.off");
+    */
+
+    /*
+
+    Sculptor pumpkin(40,40,40);
+
+    pumpkin.putEllipsoid(10,10,10,12,8,12);
+
+    pumpkin.cutEllipsoid(10,10,10,10,6,10);
+
+    pumpkin.writeOFF("pumpkin.off");
+    */
+
+    return 0;
+}
