6c831e47c953bc529280bf419ae75f5dad1a51ce
allysonliveira <allysonmgo@gmail.com>
2022-06-18 23:00:45 -0300

    atualização yoshi

--- main.cpp
+++ main.cpp
@@ -3,49 +3,63 @@
 
 int main()
 {
-    /*
-    Sculptor steve(50,50,50);
+    Sculptor yoshi(50,50,50);
 
-    //falta colorir corretamente
 
-    //cabeça
-    steve.setColor(1.0,1.0,1.0,1.0); // branco
-    steve.putBox(5,13,20,28,6,14);
 
-    //tronco
-    steve.setColor(0,0,0,1.0); // preto
-    steve.putBox(5,13,10,20,6,14);
+    //pés
+    yoshi.setColor(1,0.54,0,1);//marrom
+    yoshi.putBox(4,7,1,4,10,16);
+    yoshi.putBox(10,13,1,4,10,16);
+    //sola do pe
+    yoshi.setColor(1,1,1,1);//branco
+    yoshi.putBox(4,7,0,1,10,16);
+    yoshi.putBox(10,13,0,1,10,16);
 
     //pernas
-    steve.setColor(1.0,1.0,1.0,1.0); // branco
-    steve.putBox(5,13,2,10,6,14);
+    yoshi.setColor(0.39,0.66,0.22,1.0);//verdinho
+    yoshi.putBox(4,7,4,6,10,13);
+    yoshi.putBox(10,13,4,6,10,13);
 
-    //pes
-    steve.setColor(0,0,0,1.0); // preto
-    steve.putBox(5,13,0,2,6,15);
+    //barriguinha
+    yoshi.putBox(4,13,6,15,9,18);
+    yoshi.setColor(1,1,1,1);//branco
+    yoshi.putBox(5,12,6,15,9,18);
 
     //braços
+    yoshi.setColor(0.39,0.66,0.22,1.0);//verdinho
+    yoshi.putBox(2,4,10,14,10,12);
+    yoshi.putBox(13,15,10,14,10,12);
+    //mãozinhas
+    yoshi.setColor(0.19,0.19,0.19,1.0);//cinza
+    yoshi.putBox(2,4,7,10,10,13);
+    yoshi.putBox(13,15,7,10,10,13);
 
-    steve.setColor(1.0,1.0,1.0,1.0); // branco
-    steve.putBox(1,5,10,20,7,13);
+    //cabeça
+    yoshi.setColor(0.39,0.66,0.22,1.0);//verdinho
+    yoshi.putBox(3,14,15,28,8,15);
+    yoshi.putEllipsoid(8,19,15,7,4,6);//boca
 
-    steve.setColor(1.0,1.0,1.0,1.0); // branco
-    steve.putBox(13,17,10,20,7,13);
+    yoshi.setColor(1,1,1,1);//branco
+    yoshi.putSphere(4,19,12,4);//bochechas
+    yoshi.putSphere(12,19,12,4);//bochechas
+
+    //olhos
+    yoshi.setColor(1,1,1,1);//branco
+    yoshi.putBox(5,8,23,27,15,16);
+    yoshi.putBox(9,12,23,27,15,16);
+
+    yoshi.setColor(0,0,0,1.0);//preto
+    yoshi.putBox(6,7,23,26,15,16);
+    yoshi.putBox(10,11,23,26,15,16);
+
+    ////
+    //fazer a carapaça, calda e cascos
+    ////
 
 
-    steve.writeOFF("steve.off");
-    */
 
-    /*
+    yoshi.writeOFF("yoshi.off");
 
-    Sculptor pumpkin(40,40,40);
-
-    pumpkin.putEllipsoid(10,10,10,12,8,12);
-
-    pumpkin.cutEllipsoid(10,10,10,10,6,10);
-
-    pumpkin.writeOFF("pumpkin.off");
-    */
-
-    return 0;
+        return 0;
 }
